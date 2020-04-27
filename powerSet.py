10c2a7e667d7b152eed9d460e3c48df4665a50b8
mcnic <mcnic@ya.ru>
2020-04-27 22:25:33 +0300

    PowerSet

---
+++ powerSet.py
@@ -0,0 +1,159 @@
+import hashlib
+import math
+import copy
+
+
+class PowerSet:
+
+    def __init__(self):
+        self.len = 20000
+        self.step = 33
+        self.slots = [None] * self.len
+        self._size = 0
+
+    def hash_fun(self, value):
+        '''
+        в качестве value поступают строки!
+        '''
+        hash = hashlib.sha1()
+        hash.update(value.encode())
+        hash = int(hash.hexdigest()[:math.ceil(
+            self.len / 16)], 16)  # hex to int
+        return hash % self.len
+
+    def seek_slot(self, value):
+        '''
+        находит индекс пустого слота для значения, или None
+        '''
+        slot = self.hash_fun(value)
+        if self.slots[slot] == value:
+            return slot
+
+        # repair collision: seek new empty slot or seek value in other slot
+        max_loop = 10 * self.len
+        while not (self.slots[slot] == None or self.slots[slot] == value):
+            max_loop -= 1
+            if max_loop < 0:
+                return None
+
+            if abs(self.step) >= self.len:
+                return None
+
+            slot += self.step
+            if slot >= self.len:
+                slot -= self.len
+        return slot
+
+    def size(self):
+        '''
+        количество элементов в множестве
+        '''
+        return self._size
+
+    def find(self, value):
+        '''
+        находит индекс слота со значением, или None
+        '''
+        slot = self.hash_fun(value)
+        max_loop = 100
+        while self.slots[slot] != value:
+            max_loop -= 1
+            if max_loop < 0:
+                return None
+
+            if abs(self.step) >= self.len:
+                return None
+
+            slot += self.step
+            if slot >= self.len:
+                slot -= self.len
+        return slot
+
+    def put(self, value):
+        slot = self.seek_slot(value)
+
+        if slot == None:
+            return None
+
+        if self.slots[slot] != value:
+            self._size += 1
+            self.slots[slot] = value
+
+    def get(self, value):
+        '''
+        возвращает True если value имеется в множестве,
+        иначе False
+        '''
+        return True if self.find(value) != None else False
+
+    def remove(self, value):
+        '''
+        возвращает True если value удалено
+        иначе False
+        '''
+        slot = self.seek_slot(value)
+
+        if slot == None:
+            return False
+
+        if self.slots[slot] == value:
+            self.slots[slot] = None
+            self._size -= 1
+            return True
+
+        return False
+
+    def intersection(self, set2):
+        '''
+        пересечение текущего множества и set2
+        '''
+        i = 0
+        new_ps = PowerSet()
+        while i < self.len:
+            if self.slots[i] != None:
+                if set2.get(self.slots[i]):
+                    new_ps.put(self.slots[i])
+            i += 1
+        return new_ps
+
+    def union(self, set2):
+        '''
+        объединение текущего множества и set2
+        '''
+        new_ps = copy.deepcopy(self)
+        for val in set2.get_val():
+            new_ps.put(val)
+        return new_ps
+
+    def difference(self, set2):
+        '''
+        разница текущего множества и set2
+        '''
+        i = 0
+        new_ps = PowerSet()
+        while i < self.len:
+            if self.slots[i] != None:
+                if not set2.get(self.slots[i]):
+                    new_ps.put(self.slots[i])
+            i += 1
+        return new_ps
+
+    def issubset(self, set2):
+        '''
+        возвращает True, если set2 есть
+        подмножество текущего множества,
+        иначе False
+        '''
+        for val in set2.get_val():
+            if not self.get(val):
+                return False
+        return True
+
+    def get_val(self):
+        i = 0
+        res = []
+        while i < self.len:
+            if self.slots[i] != None:
+                res.append(self.slots[i])
+            i += 1
+        return res
---
+++ testPowerSet.py
@@ -0,0 +1,169 @@
+import unittest
+import math
+from pprint import pprint
+import random
+import string
+import copy
+
+from powerSet import PowerSet
+
+
+class TestPowerSet(unittest.TestCase):
+
+    def seed_power_set(self, str, size, len=10):
+        '''
+        str = string. may be use 'string.ascii_uppercase + string.digits'
+        '''
+        ps = PowerSet()
+        for _ in range(size+1):
+            ps.put(''.join(random.choices(str, k=len)))
+        return ps
+
+    def test_hash_fun(self):
+        ps = PowerSet()
+
+        self.assertEqual(ps.hash_fun('fsdfhxvn980cvnxcbfvjksd'), 12791)
+        self.assertEqual(ps.hash_fun('fsdfhxvnxmcvnxcbfvjksd'), 6881)
+        self.assertEqual(ps.hash_fun('yery54n77ji'), 5482)
+        self.assertEqual(ps.hash_fun('ert43564645'), 2463)
+        self.assertEqual(ps.hash_fun('rtbktut'), 18451)
+        self.assertEqual(ps.hash_fun('ывспмкенкнк'), 1642)
+        self.assertEqual(ps.hash_fun('fsdfhxvnxmcv08905xcbfvjksd'), 17554)
+        self.assertEqual(ps.hash_fun('вкс3еме45н5  564  '), 2603)
+        self.assertEqual(ps.hash_fun('fsdfhxыапукetxmcvnxcbfvjksd'), 951)
+        self.assertEqual(ps.hash_fun('fsdfhxvnxmcvnxc5435sd'), 10532)
+        self.assertEqual(ps.hash_fun('fsdfhxvnxm5443nxcbfvjksd'), 7611)
+        # print(ps.get_slots())
+
+    def test_put(self):
+        ps = PowerSet()
+
+        ps.put('abc')
+        self.assertEqual(ps.size(), 1)
+
+        ps.put('def')
+        self.assertEqual(ps.size(), 2)
+
+        ps.put('abc')
+        self.assertEqual(ps.size(), 2)
+
+        ps.put('abc')
+        self.assertEqual(ps.size(), 2)
+
+    def test_remove(self):
+        ps = PowerSet()
+        ps.put('abc')
+        ps.put('def')
+        ps.put('ghx')
+        ps.put('cvb')
+
+        self.assertEqual(ps.remove('abc'), True)
+        self.assertEqual(ps.size(), 3)
+
+        self.assertEqual(ps.remove('abc1'), False)
+        self.assertEqual(ps.size(), 3)
+
+        self.assertEqual(ps.remove('abc'), False)
+        self.assertEqual(ps.size(), 3)
+
+        self.assertEqual(ps.remove('def'), True)
+        self.assertEqual(ps.size(), 2)
+
+        self.assertEqual(ps.remove('ghx'), True)
+        self.assertEqual(ps.size(), 1)
+
+        self.assertEqual(ps.remove('cvb'), True)
+        self.assertEqual(ps.size(), 0)
+
+    def test_get(self):
+        ps = PowerSet()
+
+        self.assertEqual(ps.get('abc'), False)
+
+        ps.put('abc')
+        ps.put('def')
+        ps.put('ghx')
+        ps.put('cvb')
+
+        self.assertEqual(ps.get('abc'), True)
+        self.assertEqual(ps.get('abc1'), False)
+        self.assertEqual(ps.get('def'), True)
+        self.assertEqual(ps.get('ghx'), True)
+        self.assertEqual(ps.get('cvb'), True)
+
+    def test_intersection(self):
+        ps1 = self.seed_power_set('abcdef', 100, 2)
+        #print('\nsize1=', ps1.size())
+        # print(ps1.get_val())
+        ps2 = self.seed_power_set('abcdef', 100, 2)
+        #print('\nsize2=', ps2.size())
+        # print(ps2.get_val())
+
+        ps = ps1.intersection(ps2)
+        #print('\nsize=', ps.size())
+        # print(ps.get_val())
+
+        for val in ps.get_val():
+            self.assertEqual(ps1.get(val), True)
+            self.assertEqual(ps1.get(val), True)
+
+        ps1 = self.seed_power_set('abcdef', 100, 2)
+        ps2 = self.seed_power_set('ghjkl', 100, 2)
+        ps = ps1.intersection(ps2)
+        self.assertEqual(ps.size(), 0)
+
+    def test_union(self):
+        ps1 = self.seed_power_set('abcdef', 100, 2)
+        ps2 = self.seed_power_set('ghjkl', 100, 2)
+
+        ps = ps1.union(ps2)
+        #print('\n1=', ps1.get_val())
+        #print('\n2=', ps2.get_val())
+        #print('\n=', ps.get_val())
+        for val in ps1.get_val():
+            self.assertEqual(ps.get(val), True)
+        for val in ps2.get_val():
+            self.assertEqual(ps.get(val), True)
+
+        ps1 = self.seed_power_set('abcdef', 100, 2)
+        ps2 = PowerSet()
+        ps = ps1.union(ps2)
+        self.assertEqual(ps1.size(), ps.size())
+
+    def test_difference(self):
+        ps1 = self.seed_power_set('abcdef', 100, 2)
+        ps2 = self.seed_power_set('ghjkl', 100, 2)
+
+        ps = ps1.difference(ps2)
+        for val in ps.get_val():
+            self.assertEqual(ps1.get(val), True)
+            self.assertEqual(ps2.get(val), False)
+
+        ps1 = PowerSet()
+        ps2 = self.seed_power_set('ghjkl', 100, 2)
+        ps = ps1.difference(ps2)
+        self.assertEqual(ps.size(), 0)
+
+        ps1 = self.seed_power_set('ghjkl', 100, 2)
+        ps2 = copy.deepcopy(ps1)
+        ps = ps1.difference(ps2)
+        self.assertEqual(ps.size(), 0)
+
+    def test_issubset(self):
+        ps1 = PowerSet()
+        ps1.put('abc')
+        ps1.put('def')
+        ps1.put('ghx')
+        ps1.put('cvb')
+
+        ps2 = PowerSet()
+        self.assertEqual(ps1.issubset(ps2), True)
+
+        ps2.put('abc')
+        self.assertEqual(ps1.issubset(ps2), True)
+
+        ps2 = copy.deepcopy(ps1)
+        self.assertEqual(ps1.issubset(ps2), True)
+
+        ps2.put('ppp')
+        self.assertEqual(ps1.issubset(ps2), False)
